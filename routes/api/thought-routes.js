15632560ac9dcac3f5cffacc5be4b9e100b3b7f0
Roy <loganroyjr4@gmail.com>
2022-10-25 18:34:42 -0500

    added remaining thought routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -12,7 +12,14 @@ const {
 // /api/thoughts
 router.route('/').get(getThoughts).post(createThought);
 
-// /api/users/:thoughtId
-router.route('/:thoughtId').get(getSingleThought);
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId')
+.get(getSingleThought)
+.put(updateThought)
+.delete(deleteThought);
+
+// api/thoughts/:thoughtsId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
