68ae781bf9ec920f98347f77eb5ec4e6ea559a9b
Roy <loganroyjr4@gmail.com>
2022-10-25 17:59:51 -0500

    fixed remove friend function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -69,7 +69,7 @@ module.exports = {
   removeFriend(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
-      { $pull: { friends: { friendsId: req.params.friendsId } } },
+      { $pull: { friends: req.params.friendsId } },
       { runValidators: true, new: true }
     )
       .then((user) =>
